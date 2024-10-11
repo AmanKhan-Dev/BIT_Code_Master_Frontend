cd559d6ed1d54b7eb71de01ff954ea1eb0a286d4
AmanKhan-Dev <amankhan7058int@gmail.com>
2024-10-11 13:43:44 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0', // This allows access from any IP
-    port: 5173, // You can choose any port
+    host: '0.0.0.0', // Allows access from any IP during local development
+    port: 5173,      // You can choose any port for local development
   },
   plugins: [react()],
-
-})
+  base: '/', // Ensure the base path is set correctly for deployment
+});
